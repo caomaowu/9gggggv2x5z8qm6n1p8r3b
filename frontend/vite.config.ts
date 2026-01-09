85b6c3593da0098ff6c575d4909b5df5465f69b0
hello <user@example.com>
2026-01-09 16:27:02 +0800

    访问

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import fs from 'fs'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +10,12 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, '../caomao.xyz-ssl-bundle/private.key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, '../caomao.xyz-ssl-bundle/domain.cert.pem'))
+    }
+  }
 })
