da0a4a9655a4bbb0ac83585b231c2c6665fc15aa
hello <user@example.com>
2026-01-09 17:43:02 +0800

    适配vps部署

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,9 +13,10 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
-    https: {
+    https: fs.existsSync(path.resolve(__dirname, '../caomao.xyz-ssl-bundle/private.key.pem')) && 
+           fs.existsSync(path.resolve(__dirname, '../caomao.xyz-ssl-bundle/domain.cert.pem')) ? {
       key: fs.readFileSync(path.resolve(__dirname, '../caomao.xyz-ssl-bundle/private.key.pem')),
       cert: fs.readFileSync(path.resolve(__dirname, '../caomao.xyz-ssl-bundle/domain.cert.pem'))
-    }
+    } : undefined
   }
 })
